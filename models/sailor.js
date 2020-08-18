8683a6b0790f013be6b1cdb9ea656e93fab5c1a2
Katie Breuil <kcbreuil@gmail.com>
2020-08-18 17:59:39 -0400

    commit

--- models/sailor.js
+++ models/sailor.js
@@ -3,10 +3,48 @@ const mongoose = require('mongoose');
 const sailorSchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
+    trim: true
+  },
+  japanese_name: {
+    type: String,
     required: true
   },
-  japanese: {
+  residency: {
+    type: Array
+  },
+  occupations: {
     type: String
+  },
+  alignment: {
+    type: Array
+  },
+  aliases: {
+    type: Array,
+    required: true
+  },
+  relationships: {
+    type: Array,
+    required: true
+  },
+  birthdate: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  gender: {
+    enum: ['male', 'female', 'non-binary']
+  },
+  likes: {
+    type: String,
+    required: true
+  },
+  dislikes: {
+    type: String,
+    required: true
   }
 });
 
